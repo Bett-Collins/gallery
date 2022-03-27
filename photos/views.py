1a94d244707f3b6cb3fb3d4bd15d767bbbec9e3b
Bett-C <collins.kkiprotich.student@moringaschool.com>
2022-03-28 02:05:27 +0300

    updating views

--- photos/views.py
+++ photos/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .models import Category,Image,Location
 # Create your views here.
 #................
@@ -24,3 +24,25 @@ def viewPhoto(request,pk=int):
 def about(request):
     
     return render(request,'about.html')
+
+def addPhoto(request):
+    categories= Category.objects.all()
+
+    if request.method == 'POST':
+        data=request.POST
+        image=request.FILES.get('image')
+
+        if data['category']!='none':
+            category=Category.objects.get(id=data['category'])
+
+        elif data['category_new']!='':
+            category, created=Category.objects.get_or_create(name=data['category_new'])
+        else:
+            category=None
+
+        photo = Image.objects.create(
+            category=category,
+            description=data['description'],
+            image=image
+        )
+        return redirect('gallery')
