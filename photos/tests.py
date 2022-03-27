56a7561d9a99b22957071ff582b18400a03a01af
Bett-C <collins.kkiprotich.student@moringaschool.com>
2022-03-28 01:48:22 +0300

    updating models

--- photos/tests.py
+++ photos/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
-
+from .models import Editor,Article,tags
 # Create your tests here.
+
+class EditorTestClass(TestCase):
+
+    # Set up method
+    def setUp(self):
+        self.Collins= Editor(first_name = 'Collins', last_name ='Bett', email ='arapbett1996@gmail.com')
+        
+        # Testing  instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.Collins,Editor))
