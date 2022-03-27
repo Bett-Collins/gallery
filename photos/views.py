5b69a22ff6e077f9b95b0594083fb8e30a88d013
Bett-C <collins.kkiprotich.student@moringaschool.com>
2022-03-28 00:26:50 +0300

    adding templates

--- photos/views.py
+++ photos/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
-from django.http  import HttpResponse
+# from django.http  import HttpResponse
 
 # Create your views here.
+#................
+# Create your views here.
 def welcome(request):
-    return HttpResponse('Welcome to the Moringa Tribune')
+    return render(request, 'welcome.html')
 
