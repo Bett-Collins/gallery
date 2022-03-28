f8d5f3630e95aa726181efbb7974fb9a1d24b28a
Bett-C <collins.kkiprotich.student@moringaschool.com>
2022-03-28 22:56:09 +0300

    updating

--- photos/urls.py
+++ photos/urls.py
@@ -6,7 +6,7 @@ from django.conf.urls.static import static
 
 urlpatterns=[
    url('^$',views.gallery,name='gallery'),
-    # url('photo/<str:pk>',views.viewPhoto,name='photo'),
+    url('photo/<str:pk>',views.viewPhoto,name='photo'),
     url('add',views.addPhoto,name='add'),
     url('about',views.about,name='about'),
     url(r'^search/', views.search_results, name='search_results')
